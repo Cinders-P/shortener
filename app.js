b5f744d342ff311454ffbcc39347e5200211adef
Cinders <askcinders@outlook.com>
2016-07-09 13:21:09 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,83 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var express = require('express');
+var path = require('path');
+var validator = require('validator');
+var url = 'mongodb://test:test1@ds017185.mlab.com:17185/api';
+
+
+var app = express();
+app.enable('trust proxy');
+app.listen(process.env.PORT || 3000, function() { //need to adapt port to work correctly on heroku
+    console.log("URL shortener listening on port 3000.");
+});
+
+app.get('/new/*', function(req, res) {
+    var doc = {};
+    var suspect = req.originalUrl.slice(5);
+    if (validator.isURL(suspect)) {
+        //put the new pair into database, reply with JSON of what was added
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            if (err)
+                console.log("Fire!");
+            else {
+                db.collection('urls').find({
+                    "docs": {
+                        $exists: true
+                    }
+                }).toArray(function(err, docs) {
+                    doc = {
+                        "original": suspect,
+                        "short": req.protocol + '://' + req.get('host') + '/' + docs[0].docs
+                    };
+                    res.send(JSON.stringify(doc));
+                    db.collection('urls').insertOne(doc);
+                    console.log("Document successfully added.");
+                });
+                db.collection('urls').updateOne({
+                    "docs": {
+                        $exists: true
+                    }
+                }, {
+                    $inc: {
+                        "docs": 1
+                    }
+                }, function(err, results) {
+                    assert.equal(err, null);
+                    db.close();
+                });
+            }
+        });
+    } else {
+        res.end("Invalid or malformed URL. Please make sure you're including the protocol (e.g. http://).");
+    }
+});
+
+app.use('/:shortForm', function(req, res) {
+    if (typeof + req.params.shortForm !== "number") {
+        res.end("That is not a valid URL. If you're looking to convert a URL, use this format: " + req.protocol + '://' + req.get('host') + '/new/<your-long-url-here>');
+    } else {
+        MongoClient.connect(url, function(err, db) {
+            assert.equal(null, err);
+            db.collection('urls').findOne({
+                "short":  req.protocol + '://' + req.get('host') + req.originalUrl
+            }, function(err, doc) {
+                if (doc) {
+					console.log("Successful redirect to " + doc.original);
+                    res.redirect(doc.original);
+                } else {
+                    res.end("No URL match found. If you're looking to convert a URL, use this format: " + req.protocol + '://' + req.get('host') + '/new/<your-long-url-here>');
+                }
+            });
+        });
+    }
+});
+
+
+
+app.use(express.static(path.join(__dirname + '/public')));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
